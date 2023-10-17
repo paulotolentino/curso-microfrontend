e5ccbf111f38cc237d29147f11d4cbf73b050c68
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2023-10-17 01:06:59 -0300

    Singleton loading

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -13,7 +13,15 @@ module.exports = {
       exposes: {
         "./ProductsIndex": "./src/index.js",
       },
-      shared: ["faker"],
+      // This approach makes faker be loaded just one time if it is the same version,
+      // If not, it will load as much times as necessary
+      // shared: ["faker"],
+      // This approach makes faker be loaded just one time, no matter different versions
+      shared: {
+        faker: {
+          singleton: true,
+        },
+      },
     }),
     new HtmlWebpackPlugin({
       template: "/public/index.html",
